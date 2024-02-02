acafd800e272c1c738100d01abb1eabe2a021fff
Agustin Mamani <agusdmu1999@gmail.com>
2024-02-01 23:01:47 -0500

    Agregado juego de señas

--- project.py
+++ project.py
@@ -4,14 +4,14 @@ from asistente import AsistenteVoz
 from camera_senias import Camera
 from aprendizaje import Aprendizaje
 from test import Test
-
+from juego_ia import Juego_senias
 class VirtualAssistant:
     def __init__(self):
         self.asistente_voz = AsistenteVoz()
         self.mi_aprendizaje = Aprendizaje(callback=self.presentar_opciones)
         self.mi_test = Test()
         self.mi_camara = Camera()
-
+        self.juego = Juego_senias()
     def texto_a_audio(self, comando):
         self.asistente_voz.texto_a_audio(comando)
 
@@ -48,7 +48,7 @@ class VirtualAssistant:
 
                 elif respuesta == "juego":
                     print("\n INICIALIZANDING...")
-                    self.mi_camara.ejecutar()
+                    self.juego.ejecutar()
                 break
 
             else:
